ae20c9d8574a809980dd0369707b7331801692e8
MCTIC <alexandre.ps1123@gmail.com>
2019-11-20 08:30:24 -0300

    aula38

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
-// um componente funcional eh reutilizavel e configuravel
-// esse tipo de pratica facilita a manutencao de uma aplicacao
+/*
+    um componente funcional eh reutilizavel e configuravel
+    esse tipo de pratica facilita a manutencao de uma aplicacao
+    
+    a palavra reservada 'children' faz referencia a qualquer elemento
+    que esteja entre a abertura e fechamento de uma tag, nesse caso:
+    <Person>"Elemento que props.children ira mostrar"</Person>
+*/
 
 const person = (props) => {
-    return <p>I'm {props.name} and I'm {props.age} years old!</p>
+    return  (
+    <div>
+        <p>I'm {props.name} and I am {props.age} years old!</p>
+        <p>{props.children}</p>
+        
+    </div>
+    )
 };
 
 export default person;
