09ebbdd233c403afda83f75fe308c9109e81088e
MCTIC <alexandre.ps1123@gmail.com>
2019-11-21 08:31:24 -0300

    aula44 -BaseSyntax

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -9,14 +9,13 @@ import React from 'react';
     <Person>"Elemento que props.children ira mostrar"</Person>
 */
 
-const person = (props) => {
+const Person = (props) => {
     return  (
     <div>
         <p>I'm {props.name} and I am {props.age} years old!</p>
-        <p>{props.children}</p>
-        
+        <p>{props.children}</p>      
     </div>
     )
 };
 
-export default person;
+export default Person;
